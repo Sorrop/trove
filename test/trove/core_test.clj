53e3eb16110db322dec944fd35d7a0415e0a0649
Sorrop <chris.klaou@gmail.com>
2018-01-28 16:31:08 +0200

    Add tests for actual cache storing and cache limit size

--- test/trove/core_test.clj
+++ test/trove/core_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [trove.core :refer :all]))
 
+(defn inspect [c-fun]
+  (-> (meta c-fun)
+      :cache
+      (fetch)))
+
+(defn size-of-cache [c-fun]
+  (-> (inspect c-fun)
+      deref
+      count))
+
+(defn hypotenuse
+  [a b]
+  (java.lang.Math/sqrt (+ (* a a) (* b b))))
+
+(def cached-hyp
+  (cached-fn hypotenuse 100))
+
 (defn fact [n]
   (if (= 1 n)
     1
@@ -10,12 +27,27 @@
 (def cached-fact
   (cached-fn fact 100))
 
-(comment
-  (-> (meta cached-fact)
-      :cache
-      (fetch)
-      deref))
+(defn cached? [c-fn & args]
+  (let [res (apply c-fn args)]
+    (= (-> (meta c-fn)
+           :cache
+           (search args))
+       res)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn respects-limit?
+  [fun limit input excess]
+  (let [c-fun (cached-fn fun limit)]
+    (doseq [args input]
+      (apply c-fun args))
+    (apply c-fun excess)
+    (= 1 (size-of-cache c-fun))))
+
+(comment
+  (-> (meta cached-hyp)
+      :cache
+      (search [3 4])))
+
+(deftest basic-test
+  (testing "Test caching facilities"
+    (is (cached? cached-hyp 3 4))
+    (is (respects-limit? hypotenuse 3 [[3 4] [5 12] [6 8]] [21 28]))))
