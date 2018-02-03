720fcecf12457c2d3b6edeaca83bb2c3a6b74de2
Sorrop <chris.klaou@gmail.com>
2018-02-03 15:14:05 +0200

    Refactor tests and add one for checking if atomic cache implements fifo replacement policy

--- test/trove/core_test.clj
+++ test/trove/core_test.clj
@@ -2,15 +2,21 @@
   (:require [clojure.test :refer :all]
             [trove.core :refer :all]))
 
-(defn inspect [c-fun]
+(defn inspect-cached [c-fun]
   (-> (meta c-fun)
       :cache
-      (fetch)))
+      (fetch)
+      deref))
 
 (defn size-of-cache [c-fun]
-  (-> (inspect c-fun)
-      deref
+  (-> (inspect-cached c-fun)
+      :mappings
       count))
+
+(defn peek-stored [c-fun]
+  (-> (inspect-cached c-fun)
+      :stored
+      first))
 
 (defn hypotenuse
   [a b]
@@ -34,13 +40,28 @@
            (search args))
        res)))
 
+(defn apply-seq [c-fun input]
+  (doseq [args input]
+    (apply c-fun args)))
+
 (defn respects-limit?
   [fun limit input excess]
   (let [c-fun (cached-fn fun limit)]
-    (doseq [args input]
-      (apply c-fun args))
+    (apply-seq c-fun input)
     (apply c-fun excess)
-    (= 1 (size-of-cache c-fun))))
+    (= limit (size-of-cache c-fun))))
+
+(defn fifo?
+  [fun limit input excess]
+  (let [c-fun        (cached-fn fun limit)
+        fin          (first input)
+        first-in?    (do (apply-seq c-fun input)
+                         (= fin (peek-stored c-fun)))
+        first-out?   (do (apply c-fun excess)
+                         (not= fin (peek-stored c-fun)))]
+    (and first-in? first-out?)))
+
+
 
 (comment
   (-> (meta cached-hyp)
@@ -50,4 +71,5 @@
 (deftest basic-test
   (testing "Test caching facilities"
     (is (cached? cached-hyp 3 4))
-    (is (respects-limit? hypotenuse 3 [[3 4] [5 12] [6 8]] [21 28]))))
+    (is (respects-limit? hypotenuse 3 [[3 4] [5 12] [6 8]] [21 28]))
+    (is (fifo? hypotenuse 3 [[3 4] [5 12] [6 8]] [21 28]))))
