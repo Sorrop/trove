8c2832978bcc604dd8eaccc4db5383f6d82ec20d
Sorrop <chris.klaou@gmail.com>
2018-02-03 14:43:50 +0200

    Implement correct least recently stored semantics to atomic cache by using :stored vec as a queue

--- src/trove/core.clj
+++ src/trove/core.clj
@@ -11,15 +11,20 @@
   (search [self args] (get-in @atm [:mappings args]))
   (clean [self] (let [{:keys [mappings
                               stored]} @atm
+                      csize            (count mappings)
                       lru              (first stored)]
                   (when (= (count mappings) limit)
                     (reset! atm {:mappings (dissoc mappings lru)
-                                 :stored   (rest stored)}))))
-  (store [self args output] (let [{:keys [mappings stored]} @atm]
-                              (clean self)
-                              (reset! atm
-                                      {:mappings (assoc mappings args output)
-                                       :stored   (conj stored args)})))
+                                 :stored   (subvec stored 1 csize)}))))
+  (store [self args output] (let [{:keys [mappings stored]} @atm
+                                  cleaned                   (clean self)]
+                              (if (nil? cleaned)
+                                (reset! atm
+                                        {:mappings (assoc mappings args output)
+                                         :stored   (conj stored args)})
+                                (reset! atm (-> (assoc-in cleaned
+                                                          [:mappings args] output)
+                                                (assoc :stored (conj (:stored cleaned) args)))))))
   (fetch [self] atm))
 
 (defn cached-fn [function limit]
