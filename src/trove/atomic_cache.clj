6b62dbb561cc78681f6cf263469017ba0d2835a2
Sorrop <chris.klaou@gmail.com>
2018-02-03 18:01:13 +0200

    Merge store & replace functions of atomic cache and abstract them into a multimethod for adding other replacement policies

--- src/trove/atomic_cache.clj
+++ src/trove/atomic_cache.clj
@@ -2,27 +2,36 @@
 
 (defprotocol atomic-cache
   (search [self args])
-  (clean [self])
+  (size   [self])
+  (full?  [self])
   (store [self args output])
   (fetch [self]))
 
-(deftype fifo-cache [atm limit]
+(defmulti sequential-replace! (fn [policy atm csize args output] policy))
+
+(defmethod sequential-replace! :fifo
+  [policy atm csize args output]
+  (let [{:keys [mappings stored]} @atm
+        first-in         (first stored)]
+    (reset! atm {:mappings (-> (dissoc mappings first-in)
+                               (assoc args output))
+                 :stored   (-> (subvec stored 1 csize)
+                               (conj args))})))
+
+(defmethod sequential-replace! :default [_] "error")
+
+
+(deftype sequential-cache [atm limit policy]
   atomic-cache
   (search [self args] (get-in @atm [:mappings args]))
-  (clean [self] (let [{:keys [mappings
-                              stored]} @atm
-                      csize            (count mappings)
-                      lru              (first stored)]
-                  (when (= (count mappings) limit)
-                    (reset! atm {:mappings (dissoc mappings lru)
-                                 :stored   (subvec stored 1 csize)}))))
-  (store [self args output] (let [{:keys [mappings stored]} @atm
-                                  cleaned                   (clean self)]
-                              (if (nil? cleaned)
+  (size [self] (-> @atm :mappings count))
+  (full?  [self] (= (size self) limit))
+  (store [self args output] (let [{:keys [mappings stored]} @atm]
+                              (if (full? self)
+                                (sequential-replace! policy atm (size self) args output)
                                 (reset! atm
                                         {:mappings (assoc mappings args output)
-                                         :stored   (conj stored args)})
-                                (reset! atm (-> (assoc-in cleaned
-                                                          [:mappings args] output)
-                                                (assoc :stored (conj (:stored cleaned) args)))))))
+                                         :stored   (conj stored args)}))))
   (fetch [self] atm))
+
+
--- src/trove/core.clj
+++ src/trove/core.clj
@@ -1,14 +1,19 @@
 (ns trove.core
   (:require [trove.atomic-cache :as cache]))
 
+(defn cached [function cstore]
+  (with-meta
+    (fn [& args]
+      (or (.search cstore args)
+          (let [res (apply function args)]
+            (.store cstore args res)
+            res)))
+    {:cache cstore}))
+
 
 (defn cached-fn [function limit]
-  (let [cstore (cache/->fifo-cache (atom {:mappings {}
-                                          :stored   []}) limit)]
-    (with-meta
-      (fn [& args]
-        (or (.search cstore args)
-            (let [res (apply function args)]
-              (.store cstore args res)
-              res)))
-      {:cache cstore})))
+  (let [cstore (cache/->sequential-cache (atom {:mappings {}
+                                                :stored   []})
+                                         limit
+                                         :fifo)]
+    (cached function cstore)))
